1998a786a901f5f50a4c1401d722f82fe274c69c
Evgenii Kireev <kireev228@gmail.com>
2025-02-19 07:18:00 -0300

    added gas report & natspec

--- src/TokenVestingOptimized.sol
+++ src/TokenVestingOptimized.sol
@@ -1,117 +1,156 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Ownable } from '../lib/solady/src/auth/Ownable.sol';
-import { SafeTransferLib } from '../lib/solady/src/utils/SafeTransferLib.sol';
-import { Test, console } from 'forge-std/Test.sol';
+import {Ownable} from "../lib/solady/src/auth/Ownable.sol";
+import {SafeTransferLib} from "../lib/solady/src/utils/SafeTransferLib.sol";
 
 interface IERC20 {
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address account) external view returns (uint256);
-  function transfer(address to, uint256 value) external returns (bool);
-  function allowance(address owner, address spender) external view returns (uint256);
-  function approve(address spender, uint256 value) external returns (bool);
-  function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
+/**
+ * @title TokenVestingOptimized
+ * @notice A smart contract for token vesting with an optional revocable feature.
+ * @dev Uses SafeTransferLib for secure token transfers.
+ */
 contract TokenVestingOptimized is Ownable {
-  using SafeTransferLib for address;
+    using SafeTransferLib for address;
 
-  event TokensReleased(address token, uint256 amount);
-  event TokenVestingRevoked(address token);
+    event TokensReleased(address token, uint256 amount);
+    event TokenVestingRevoked(address token);
 
-  address public beneficiary;
-  bool public revocable;
+    address public beneficiary;
+    bool public revocable;
 
-  uint256 public cliff;
-  uint256 public start;
-  uint256 public duration;
+    uint256 public cliff;
+    uint256 public start;
+    uint256 public duration;
 
-  mapping(address => uint256) private released;
-  mapping(address => bool) private revoked;
+    mapping(address => uint256) private released;
+    mapping(address => bool) private revoked;
 
-  constructor(address _beneficiary, uint256 _start, uint256 _cliff, uint256 _duration, bool _revocable) {
-    require(_beneficiary != address(0), 'TokenVesting: beneficiary is the zero address');
-    require(_cliff <= _duration, 'TokenVesting: cliff is longer than duration');
-    require(_duration > 0, 'TokenVesting: duration is 0');
-    require(_start + _duration > block.timestamp, 'TokenVesting: final time is before current time');
+    /**
+     * @notice Initializes the vesting contract.
+     * @param _beneficiary The address of the beneficiary.
+     * @param _start The start time of the vesting schedule.
+     * @param _cliff The cliff duration in seconds.
+     * @param _duration The total duration of the vesting.
+     * @param _revocable Whether the vesting can be revoked by the owner.
+     */
+    constructor(address _beneficiary, uint256 _start, uint256 _cliff, uint256 _duration, bool _revocable) {
+        require(_beneficiary != address(0), "TokenVesting: beneficiary is the zero address");
+        require(_cliff <= _duration, "TokenVesting: cliff is longer than duration");
+        require(_duration > 0, "TokenVesting: duration is 0");
+        require(_start + _duration > block.timestamp, "TokenVesting: final time is before current time");
 
-    beneficiary = _beneficiary;
-    revocable = _revocable;
-    duration = _duration;
-    cliff = _start + _cliff;
-    start = _start;
-    _initializeOwner(msg.sender);
-  }
-
-  function getReleased(address token) public view returns (uint256 value) {
-    value = released[token];
-  }
-
-  function getRevoked(address token) public view returns (bool value) {
-    value = revoked[token];
-  }
-
-  function release(IERC20 token) public {
-    uint256 unreleased = _releasableAmount(token);
-
-    require(unreleased > 0, 'TokenVesting: no tokens are due');
-
-    released[address(token)] = released[address(token)] + unreleased;
-
-    address(token).safeTransfer(beneficiary, unreleased);
-
-    emit TokensReleased(address(token), unreleased);
-  }
-
-  function revoke(IERC20 token) public onlyOwner {
-    require(revocable, 'TokenVesting: cannot revoke');
-    require(!revoked[address(token)], 'TokenVesting: token already revoked');
-
-    uint256 balance = token.balanceOf(address(this));
-    uint256 unreleased = _releasableAmount(token);
-    uint256 refund = balance - unreleased;
-
-    revoked[address(token)] = true;
-    address(token).safeTransfer(owner(), refund);
-
-    emit TokenVestingRevoked(address(token));
-  }
-
-  function emergencyRevoke(IERC20 token) public onlyOwner {
-    require(revocable, 'TokenVesting: cannot revoke');
-    require(!revoked[address(token)], 'TokenVesting: token already revoked');
-
-    uint256 balance = token.balanceOf(address(this));
-
-    revoked[address(token)] = true;
-
-    address(token).safeTransfer(owner(), balance);
-
-    emit TokenVestingRevoked(address(token));
-  }
-
-  function _releasableAmount(IERC20 token) private view returns (uint256 amount) {
-    uint256 tokenReleased = released[address(token)];
-    uint256 currentBalance = token.balanceOf(address(this));
-    uint256 totalBalance = currentBalance + tokenReleased;
-    uint256 timeStamp = block.timestamp;
-
-    uint256 vestedAmount;
-    uint256 _start = start;
-    uint256 _duration = duration;
-
-    if (timeStamp < cliff) {
-      vestedAmount = 0;
-    } else if (timeStamp >= _start + _duration || revoked[address(token)]) {
-      vestedAmount = totalBalance;
-    } else {
-      vestedAmount = (totalBalance * (timeStamp - _start)) / _duration;
+        beneficiary = _beneficiary;
+        revocable = _revocable;
+        duration = _duration;
+        cliff = _start + _cliff;
+        start = _start;
+        _initializeOwner(msg.sender);
     }
 
-    amount = vestedAmount - tokenReleased;
-  }
+    /**
+     * @notice Returns the amount of tokens already released for a given token.
+     * @param token The token address.
+     * @return value The amount of tokens released.
+     */
+    function getReleased(address token) public view returns (uint256 value) {
+        value = released[token];
+    }
+
+    /**
+     * @notice Checks if a token has been revoked.
+     * @param token The token address.
+     * @return value True if revoked, false otherwise.
+     */
+    function getRevoked(address token) public view returns (bool value) {
+        value = revoked[token];
+    }
+
+    /**
+     * @notice Releases vested tokens to the beneficiary.
+     * @param token The token to release.
+     */
+    function release(IERC20 token) public {
+        uint256 unreleased = _releasableAmount(token);
+
+        require(unreleased > 0, "TokenVesting: no tokens are due");
+
+        released[address(token)] = released[address(token)] + unreleased;
+
+        address(token).safeTransfer(beneficiary, unreleased);
+
+        emit TokensReleased(address(token), unreleased);
+    }
+
+    /**
+     * @notice Revokes the vesting of a token, transferring remaining balance to the owner.
+     * @param token The token to revoke.
+     */
+    function revoke(IERC20 token) public onlyOwner {
+        require(revocable, "TokenVesting: cannot revoke");
+        require(!revoked[address(token)], "TokenVesting: token already revoked");
+
+        uint256 balance = token.balanceOf(address(this));
+        uint256 unreleased = _releasableAmount(token);
+        uint256 refund = balance - unreleased;
+
+        revoked[address(token)] = true;
+        address(token).safeTransfer(owner(), refund);
+
+        emit TokenVestingRevoked(address(token));
+    }
+
+    /**
+     * @notice Emergency revocation of vesting, transferring all remaining tokens to the owner.
+     * @param token The token to revoke.
+     */
+    function emergencyRevoke(IERC20 token) public onlyOwner {
+        require(revocable, "TokenVesting: cannot revoke");
+        require(!revoked[address(token)], "TokenVesting: token already revoked");
+
+        uint256 balance = token.balanceOf(address(this));
+
+        revoked[address(token)] = true;
+
+        address(token).safeTransfer(owner(), balance);
+
+        emit TokenVestingRevoked(address(token));
+    }
+
+    /**
+     * @notice Calculates the amount of vested tokens that can be released.
+     * @param token The token to calculate for.
+     * @return amount The amount of releasable tokens.
+     */
+    function _releasableAmount(IERC20 token) private view returns (uint256 amount) {
+        uint256 tokenReleased = released[address(token)];
+        uint256 currentBalance = token.balanceOf(address(this));
+        uint256 totalBalance = currentBalance + tokenReleased;
+        uint256 timeStamp = block.timestamp;
+
+        uint256 vestedAmount;
+        uint256 _start = start;
+        uint256 _duration = duration;
+
+        if (timeStamp < cliff) {
+            vestedAmount = 0;
+        } else if (timeStamp >= _start + _duration || revoked[address(token)]) {
+            vestedAmount = totalBalance;
+        } else {
+            vestedAmount = (totalBalance * (timeStamp - _start)) / _duration;
+        }
+
+        amount = vestedAmount - tokenReleased;
+    }
 }
--- test/TokenVestingOptimized.t.sol
+++ test/TokenVestingOptimized.t.sol
@@ -1,82 +1,82 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { TokenVestingOptimized, IERC20 } from '../src/TokenVestingOptimized.sol';
-import { Test, console } from 'forge-std/Test.sol';
-import { ERC20 } from '../lib/solady/src/tokens/ERC20.sol';
+import {TokenVestingOptimized, IERC20} from "../src/TokenVestingOptimized.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {ERC20} from "../lib/solady/src/tokens/ERC20.sol";
 
 contract TestToken is ERC20 {
-  string private _name = 'Test Token';
-  string private _symbol = 'TEST';
+    string private _name = "Test Token";
+    string private _symbol = "TEST";
 
-  constructor() {
-    _mint(msg.sender, 1_000_000e18);
-  }
+    constructor() {
+        _mint(msg.sender, 1_000_000e18);
+    }
 
-  function name() public view override returns (string memory) {
-    return _name;
-  }
+    function name() public view override returns (string memory) {
+        return _name;
+    }
 
-  function symbol() public view override returns (string memory) {
-    return _symbol;
-  }
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
 }
 
 contract TokenVestingOptimizedTest is Test {
-  TokenVestingOptimized tokenVesting;
-  TestToken token1;
+    TokenVestingOptimized tokenVesting;
+    TestToken token1;
 
-  uint256 start;
-  uint256 cliff;
-  uint256 duration;
-  address beneficiary = makeAddr('beneficiary');
+    uint256 start;
+    uint256 cliff;
+    uint256 duration;
+    address beneficiary = makeAddr("beneficiary");
 
-  function setUp() public {
-    start = 1;
-    cliff = 7;
-    duration = 100;
+    function setUp() public {
+        start = 1;
+        cliff = 7;
+        duration = 100;
 
-    tokenVesting = new TokenVestingOptimized(beneficiary, start, cliff, duration, true);
-    token1 = new TestToken();
-  }
+        tokenVesting = new TokenVestingOptimized(beneficiary, start, cliff, duration, true);
+        token1 = new TestToken();
+    }
 
-  function testGetRealesed() public {
-    uint256 released = tokenVesting.getReleased(address(this));
-    assertEq(released, 0);
-  }
+    function testGetRealesed() public {
+        uint256 released = tokenVesting.getReleased(address(this));
+        assertEq(released, 0);
+    }
 
-  function testGetRevoked() public {
-    bool revoked = tokenVesting.getRevoked(address(this));
-    assertEq(revoked, false);
-  }
+    function testGetRevoked() public {
+        bool revoked = tokenVesting.getRevoked(address(this));
+        assertEq(revoked, false);
+    }
 
-  function testRelease() public {
-    vm.warp(50);
-    token1.transfer(address(tokenVesting), 1_000e18);
-    tokenVesting.release(IERC20(address(token1)));
+    function testRelease() public {
+        vm.warp(50);
+        token1.transfer(address(tokenVesting), 1_000e18);
+        tokenVesting.release(IERC20(address(token1)));
 
-    uint256 released = tokenVesting.getReleased(address(token1));
-    console.log('released', released);
-    assertEq(released, 490000000000000000000, 'released is not correct');
-  }
+        uint256 released = tokenVesting.getReleased(address(token1));
+        console.log("released", released);
+        assertEq(released, 490000000000000000000, "released is not correct");
+    }
 
-  function testRevoke() public {
-    vm.warp(50);
-    uint256 balanceBefore = token1.balanceOf(address(this));
-    token1.transfer(address(tokenVesting), 1_000e18);
-    tokenVesting.release(IERC20(address(token1)));
+    function testRevoke() public {
+        vm.warp(50);
+        uint256 balanceBefore = token1.balanceOf(address(this));
+        token1.transfer(address(tokenVesting), 1_000e18);
+        tokenVesting.release(IERC20(address(token1)));
 
-    tokenVesting.revoke(IERC20(address(token1)));
+        tokenVesting.revoke(IERC20(address(token1)));
 
-    uint256 balanceAfter = token1.balanceOf(address(this));
-    uint256 balContract = token1.balanceOf(address(tokenVesting));
-  }
+        uint256 balanceAfter = token1.balanceOf(address(this));
+        uint256 balContract = token1.balanceOf(address(tokenVesting));
+    }
 
-  function testEmergencyRevoke() public {
-    vm.warp(50);
-    token1.transfer(address(tokenVesting), 1_000e18);
-    tokenVesting.release(IERC20(address(token1)));
+    function testEmergencyRevoke() public {
+        vm.warp(50);
+        token1.transfer(address(tokenVesting), 1_000e18);
+        tokenVesting.release(IERC20(address(token1)));
 
-    tokenVesting.emergencyRevoke(IERC20(address(token1)));
-  }
+        tokenVesting.emergencyRevoke(IERC20(address(token1)));
+    }
 }
